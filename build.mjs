3dfefa6dcb8e29529a1c12806b378ece864ed210
子长 <zichang.nong@tuya.com>
2024-01-23 15:51:31 +0800

    swc transform error

---
+++ build.mjs
@@ -0,0 +1,18 @@
+import { transform } from '@swc/core';
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+const code = fs.readFileSync(path.resolve(__dirname, './a.js'), 'utf-8');
+const map = fs.readFileSync(path.resolve(__dirname, './a.js.map'), 'utf-8');
+
+const cjs = await transform(code, {
+  filename: 'a.js',
+  sourceMaps: true,
+  inputSourceMap: map,
+  jsc: { target: 'es5' },
+  module: { type: 'commonjs', strictMode: false },
+}).catch((e) => {
+  console.error('[esm2cjs]', e);
+});
